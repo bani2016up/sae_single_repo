f3b28470cc36968db0a6e17729c0a7e6c71d522b
D1ffic00lt <dm.filinov@gmail.com>
2025-04-24 18:15:25 +0300

    model is divided into 2 parts, added properties for preprocessing device

--- backend/AI_services/ai_services/models/fact_checker.py
+++ backend/AI_services/ai_services/models/fact_checker.py
@@ -1,15 +1,69 @@
-import re
+import torch
 
-from typing import List, Literal
 from tqdm.auto import tqdm
+from typing import List, Literal, Iterable, Union
 from transformers import RobertaForSequenceClassification, RobertaTokenizer
 
-from ..interfaces import FactCheckerInterface
+from ..interfaces import FactCheckerInterface, DeviceAwareModel
 from ..response import SuggestionResponse
-from ..processing import Pipeline
+from ..processing import Pipeline, get_default_paragraph_processing_pipeline
 
 
-class FactChecker(FactCheckerInterface):
+class _BaseModel(DeviceAwareModel):
+    def __init__(
+        self,
+        model_name: str = "Dzeniks/roberta-fact-check",
+        *,
+        device: Literal["cuda", "cpu"] = "cuda",
+        tokenizer_kwargs: dict = None,
+        model_kwargs: dict = None,
+        use_tqdm: bool = False
+    ):
+        super().__init__(device=device)
+        self.use_tqdm = use_tqdm
+        self.tokenizer = RobertaTokenizer.from_pretrained(
+            model_name, **(tokenizer_kwargs or {})
+        )
+        self.model = RobertaForSequenceClassification.from_pretrained(
+            model_name, **(model_kwargs or {})
+        )
+        self.model.eval()
+        self.model.to(device)
+        self.tokenizer.to(device)
+
+    def to(self, device: Literal["cpu", "cuda"]) -> "_BaseModel":
+        self.device = device
+        self.model.to(device)
+        self.tokenizer.to(device)
+        return self
+
+    def __call__(
+        self,
+        claim: Union[str, Iterable[str]],
+        evidence: Union[str, Iterable[str]]
+    ) -> torch.Tensor:
+        if isinstance(claim, str):
+            claim = [claim]
+        if isinstance(evidence, str):
+            evidence = [evidence]
+        # TODO: Handle with custom classes
+        if len(claim) != len(evidence):
+            raise ValueError("Claim and evidence must have the same length.")
+
+        inputs = self.tokenizer(
+            claim,
+            evidence,
+            return_tensors="pt"
+        ).to(self.device)
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        return outputs
+
+    forward = __call__  # just in case
+
+
+class FactChecker(FactCheckerInterface, _BaseModel):
     def __init__(
         self,
         model_name: str = "Dzeniks/roberta-fact-check",
@@ -20,37 +74,33 @@ class FactChecker(FactCheckerInterface):
         tokenizer_kwargs: dict = None,
         model_kwargs: dict = None,
         use_tqdm: bool = False,
+        paragraph_processing_device: Literal["cuda", "cpu"] = "cuda",
+        sentence_processing_device: Literal["cuda", "cpu"] = "cpu"
+
     ):
-        super().__init__(device=device)
-
-        self.model_name = model_name
-        self.tokenizer = RobertaTokenizer.from_pretrained(
-            'Dzeniks/roberta-fact-check',
-            **(tokenizer_kwargs or {})
-        )
-        self.model = RobertaForSequenceClassification.from_pretrained(
-            'Dzeniks/roberta-fact-check',
-            **(model_kwargs or {})
+        super().__init__(
+            model_name=model_name,
+            device=device,
+            tokenizer_kwargs=tokenizer_kwargs,
+            model_kwargs=model_kwargs,
+            use_tqdm=use_tqdm
         )
 
-        if paragraph_processing_pipeline is None:
-            self.paragraph_processing_pipeline = Pipeline(
-                [
-                    ("sentence_reg", re.compile(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?])\s").split)
-                ]
-            )
-        else:
-            self.paragraph_processing_pipeline = paragraph_processing_pipeline
+        self._paragraph_processing_device = paragraph_processing_device
+        self._sentence_processing_device = sentence_processing_device
 
-        self.sentence_processing_pipeline = sentence_processing_pipeline or Pipeline()
-        self.use_tqdm = use_tqdm
+        self.paragraph_processing_pipeline = (
+                paragraph_processing_pipeline or get_default_paragraph_processing_pipeline()
+        )
+        self.sentence_processing_pipeline = (
+                sentence_processing_pipeline or Pipeline()
+        )
 
-    def to(self, device: Literal["cpu", "cuda"]) -> None:
-        self.device = device
-        self.model.to(device)
-        self.tokenizer.to(device)
+        self.paragraph_processing_pipeline.to(paragraph_processing_device)
+        self.sentence_processing_pipeline.to(sentence_processing_device)
 
     def evaluate_sentence(self, sentence: str, context: str = "") -> List[SuggestionResponse]:
+        # TODO: Implement the logic to evaluate a single sentence
         pass
 
     def evaluate_text(self, text: str, *, context: str = "") -> List[SuggestionResponse]:
@@ -68,3 +118,21 @@ class FactChecker(FactCheckerInterface):
             if len(sentence) > 0:
                 result.extend(self.evaluate_sentence(sentence, context=context))
         return result
+
+    @property
+    def paragraph_processing_device(self) -> Literal["cuda", "cpu"]:
+        return self._paragraph_processing_device
+
+    @paragraph_processing_device.setter
+    def paragraph_processing_device(self, device: Literal["cuda", "cpu"]):
+        self._paragraph_processing_device = device
+        self.paragraph_processing_pipeline.to(device)
+
+    @property
+    def sentence_processing_device(self) -> Literal["cuda", "cpu"]:
+        return self._sentence_processing_device
+
+    @sentence_processing_device.setter
+    def sentence_processing_device(self, device: Literal["cuda", "cpu"]):
+        self._sentence_processing_device = device
+        self.sentence_processing_pipeline.to(device)
