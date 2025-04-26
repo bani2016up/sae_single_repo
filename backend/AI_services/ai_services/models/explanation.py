42df186e5e58f6bf8007bb115f91691ed8c714ce
D1ffic00lt <dm.filinov@gmail.com>
2025-04-27 00:15:01 +0300

    added interface for generating promts and receiving them in llm

--- backend/AI_services/ai_services/models/explanation.py
+++ backend/AI_services/ai_services/models/explanation.py
@@ -1,7 +1,9 @@
 from transformers import pipeline
-from typing import Literal
+from typing import Literal, Callable, Union
 
 from ..interfaces import DeviceAwareModel
+from ..utils import FactCheckerPrompt
+from ..interfaces import PromptInterface
 
 
 class ExplanationLLM(DeviceAwareModel):
@@ -10,10 +12,14 @@ class ExplanationLLM(DeviceAwareModel):
         model: str = "Open-Orca/Mistral-7B-OpenOrca",
         *,
         device: Literal["cuda", "cpu"] = "cuda",
+        prompt_generator: Union[PromptInterface, Callable] = None,
         torch_dtype: str = "auto",
         device_map: str = "auto",
     ) -> None:
         super().__init__(device=device)
+        if prompt_generator is None:
+            prompt_generator = FactCheckerPrompt()
+        self.prompt_generator = prompt_generator
         self.llm = pipeline(
             "text-generation", model=model,
             torch_dtype=torch_dtype,
@@ -42,12 +48,12 @@ class ExplanationLLM(DeviceAwareModel):
     def __call__(
         self, claim: str,
         evidence: str,
+        *,
         max_new_tokens: int = 256,
         do_sample: bool = False,
         temperature: float = 0.1
     ) -> str:
-        # FIXME: prompt.
-        prompt = f"Claim: {claim}\nEvidence: {evidence}\nExplain why this claim is true or false."
+        prompt = self.prompt_generator(claim, evidence)
         response = self.llm(
             prompt,
             max_new_tokens=max_new_tokens,
--- backend/AI_services/ai_services/utils.py
+++ backend/AI_services/ai_services/utils.py
@@ -1,4 +1,27 @@
+from copy import deepcopy
+from typing import List, Dict, Union
+
 from .models.fact_checker import FactCheckerPipeline
+from .interfaces import PromptInterface
+from .static import FACT_CHECKER_PROMPT
+
+
+class FactCheckerPrompt(PromptInterface):
+    def __init__(self, base_prompt: Union[str, List[Dict[str, str]]] = None) -> None:
+        if base_prompt is None:
+            base_prompt = deepcopy(FACT_CHECKER_PROMPT)
+        self.base_prompt = base_prompt
+
+    def __call__(self, claim: str, evidence: str) -> Union[str, List[Dict[str, str]]]:
+        prompt = deepcopy(self.base_prompt)
+        if isinstance(prompt, str):
+            return prompt.format(claim=claim, evidence=evidence)
+        item = prompt[-1]
+        if isinstance(item, dict):
+            item["content"] = item["content"].format(claim=claim, evidence=evidence)
+        else:
+            raise ValueError("The last item in the prompt list must be a dictionary.")
+        return prompt
 
 
 def build_fact_check_model() -> FactCheckerPipeline:
