0f196d48cb606bc329b0835b54744ae206ef58be
D1ffic00lt <dm.filinov@gmail.com>
2025-04-29 02:20:43 +0300

    added docs

--- backend/AI_services/ai_services/processing.py
+++ backend/AI_services/ai_services/processing.py
@@ -2,7 +2,7 @@ import inspect
 import re
 
 from collections import OrderedDict
-from typing import List, Tuple, Any, Callable
+from typing import Iterator, List, Tuple, Any, Callable
 from tqdm.auto import tqdm
 
 from .interfaces import DeviceAwareModel
@@ -16,6 +16,14 @@ __all__ = (
 
 
 class Pipeline(DeviceAwareModel):
+    """
+    A class to create a processing pipeline for data.
+    This class allows for the registration of multiple processing steps,
+    each represented by a callable function.
+    The pipeline can be executed in sequence, processing the data
+    through each registered step.
+    """
+
     def __init__(
         self,
         steps: List[Tuple[str, Callable]] = None,
@@ -24,6 +32,16 @@ class Pipeline(DeviceAwareModel):
         device: DeviceType = "cpu",
         **kwargs
     ):
+        """
+        Initialize the Pipeline with the specified steps and device.
+
+        Args:
+            steps (List[Tuple[str, Callable]]): A list of tuples where each tuple contains
+                a name (str) and a callable function. The functions will be executed in order.
+            use_tqdm (bool): Whether to use tqdm for progress tracking.
+            device (DeviceType): Device to load the model on ("cpu" or "cuda").
+            **kwargs: Additional keyword arguments for registering steps.
+        """
         super().__init__(device=device)
         if steps is None:
             steps = []
@@ -42,23 +60,53 @@ class Pipeline(DeviceAwareModel):
         return func
 
     def register(self, name: str, func: Callable):
+        """
+        Register a new step in the pipeline.
+
+        Args:
+            name (str): The name of the step.
+            func (Callable): The function to be executed in this step.
+        """
         if name in self.pipeline:
             raise ValueError(f"Pipeline already contains a step with name '{name}'")
         self.pipeline[name] = func
         self._func2device(func)
 
     def unregister(self, name: str):
+        """
+        Unregister a step from the pipeline.
+
+        Args:
+            name (str): The name of the step to be removed.
+        Raises:
+            ValueError: If the step with the specified name does not exist in the pipeline.
+        """
         if name not in self.pipeline:
             raise ValueError(f"Pipeline does not contain a step with name '{name}'")
         del self.pipeline[name]
 
     def to(self, device: DeviceType) -> "Pipeline":
+        """
+        Move the pipeline to the specified device.
+
+        Args:
+            device (DeviceType): The target device ("cpu" or "cuda").
+        """
         self._device = device
         for func in self.pipeline.values():
             self._func2device(func)
         return self
 
-    def __call__(self, data: Any, **kwargs) -> Any:
+    def __call__(self, data: Any) -> Any:
+        """
+        Execute the pipeline on the provided data.
+        The data is processed through each registered step in the order they were added.
+
+        Args:
+            data (Any): The input data to be processed.
+        Returns:
+            Any: The processed data after passing through the pipeline.
+        """
         iterator = tqdm(
             self.pipeline.items(),
             desc="Processing",
@@ -70,7 +118,14 @@ class Pipeline(DeviceAwareModel):
             data = func(data)
         return data
 
-    def __getitem__(self, name: str):
+    def __getitem__(self, name: str) -> Callable:
+        """
+        Get a registered step by its name.
+        Args:
+            name (str): The name of the step to retrieve.
+        Returns:
+            Callable: The function associated with the specified step name.
+        """
         return self.pipeline[name]
 
     def __repr__(self):
@@ -95,21 +150,61 @@ class Pipeline(DeviceAwareModel):
         return f"Pipeline(\n{body}\n)"
 
     def __len__(self):
+        """
+        Get the number of steps in the pipeline.
+        Returns:
+            int: The number of steps in the pipeline.
+        """
         return len(self.pipeline)
 
     def __contains__(self, name: str):
+        """
+        Check if a step with the specified name exists in the pipeline.
+
+        Args:
+            name (str): The name of the step to check.
+
+        Returns:
+            bool: True if the step exists, False otherwise.
+        """
         return name in self.pipeline
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Tuple[str, Callable]]:
+        """
+        Iterate over the steps in the pipeline.
+
+        Returns:
+            Iterator[Tuple[str, Callable]]: An iterator over the steps in the pipeline.
+        """
         return iter(self.pipeline.items())
 
-    def __getattr__(self, item):
+    def __getattr__(self, item) -> Callable:
+        """
+        Get a registered step by its name using dot notation.
+
+        Args:
+            item (str): The name of the step to retrieve.
+
+        Returns:
+            Callable: The function associated with the specified step name.
+        """
         if item in self.pipeline:
             return self.pipeline[item]
         raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
 
 
 def get_default_paragraph_processing_pipeline() -> Pipeline:
+    """
+    Create a default paragraph processing pipeline.
+    This pipeline includes a sentence segmentation step using regex.
+    It splits the text into sentences based on punctuation marks.
+    The sentences are split using a regex pattern that looks for
+    punctuation followed by whitespace.
+    The pipeline is initialized with a device set to "cpu".
+
+    Returns:
+        Pipeline: The initialized pipeline with sentence segmentation step.
+    """
     return Pipeline(
         sentence_reg=re.compile(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?])\s").split,
         device="cpu"
@@ -117,6 +212,21 @@ def get_default_paragraph_processing_pipeline() -> Pipeline:
 
 
 def get_default_coref_pipeline(*, device: DeviceType = "cuda") -> Pipeline:
+    """
+    Create a default coreference resolution pipeline.
+    This pipeline includes a coreference resolution step using the LingMessCoref model.
+    The model is initialized with the specified device.
+    The coreference resolution step processes the input text
+    and returns a list of resolved sentences.
+    The sentences are represented as `SentenceProposal` objects,
+    which contain the tokens and their indices.
+    The pipeline is initialized with the specified device.
+
+    Args:
+        device (DeviceType): The device to load the model on ("cpu" or "cuda").
+    Returns:
+        Pipeline: The initialized pipeline with coreference resolution step.
+    """
     return Pipeline(
         steps=[
             ("coref", CorefResolver()),
