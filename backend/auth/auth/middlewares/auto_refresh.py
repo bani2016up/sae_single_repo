166ddf2c1b06bd1ae49694955a21b16ba9a66c3d
Levonidor <lvb.korobkov@gmail.com>
2025-05-05 20:44:02 +0300

    funcs in auto_refresh.py -> private methods of AutoRefreshTokenMiddleware

--- backend/auth/auth/middlewares/auto_refresh.py
+++ backend/auth/auth/middlewares/auto_refresh.py
@@ -27,42 +27,45 @@ class AutoRefreshTokenMiddleware(BaseHTTPMiddleware):
         refresh_token = request.cookies.get(REFRESH_TOKEN)
 
         if not access_token and not refresh_token:
-            return await handle_no_tokens(request, call_next)
+            return await self.handle_no_tokens(request, call_next)
 
         if not access_token and refresh_token:
-            return await handle_only_refresh_token(request, call_next, refresh_token)
+            return await self.handle_only_refresh_token(
+                request, call_next, refresh_token
+            )
 
         if access_token and not refresh_token:
-            return await handle_only_access_token(request, call_next)
+            return await self.handle_only_access_token(request, call_next)
 
         return await call_next(request)
 
-
-async def handle_no_tokens(request: Request, call_next):
-    logger.info("No access or refresh token found, proceeding without authentication")
-    return await call_next(request)
-
-
-async def handle_only_refresh_token(request: Request, call_next, refresh_token: str):
-    logger.info("No access token found, proceeding update with refresh token")
-    try:
-        res = supabase.auth.refresh_session(refresh_token)
-        logger.info("Token refreshed successfully")
-        response = await call_next(request)
-        set_auth_cookies(
-            response,
-            res.session.access_token,
-            res.session.refresh_token,
+    async def handle_no_tokens(self, request: Request, call_next):
+        logger.info(
+            "No access or refresh token found, proceeding without authentication"
         )
-        return response
-    except Exception as e:
-        logger.error(f"Failed to refresh token: {str(e)}")
-        return JSONResponse(
-            status_code=401,
-            content={"detail": "Failed to refresh token"},
-        )
+        return await call_next(request)
 
+    async def handle_only_refresh_token(
+        self, request: Request, call_next, refresh_token: str
+    ):
+        logger.info("No access token found, proceeding update with refresh token")
+        try:
+            res = supabase.auth.refresh_session(refresh_token)
+            logger.info("Token refreshed successfully")
+            response = await call_next(request)
+            set_auth_cookies(
+                response,
+                res.session.access_token,
+                res.session.refresh_token,
+            )
+            return response
+        except Exception as e:
+            logger.error(f"Failed to refresh token: {str(e)}")
+            return JSONResponse(
+                status_code=401,
+                content={"detail": "Failed to refresh token"},
+            )
 
-async def handle_only_access_token(request: Request, call_next):
-    logger.info("No refresh token found, access token found. How the fu-")
-    return await call_next(request)
+    async def handle_only_access_token(self, request: Request, call_next):
+        logger.info("No refresh token found, access token found. How the fu-")
+        return await call_next(request)
