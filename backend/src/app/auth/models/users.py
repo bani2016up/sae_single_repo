6ee04d632f63ccde922bcdef8028ef16b344ecb9
Levonidor <lvb.korobkov@gmail.com>
2025-05-23 23:55:18 +0300

    new pydantic schemas for email/password change

--- backend/src/app/auth/models/users.py
+++ backend/src/app/auth/models/users.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
 
 
 class UserRegister(BaseModel):
@@ -10,6 +12,32 @@ class UserLogin(BaseModel):
     email: str
     password: str
 
+
 class UserRegisterResponse(BaseModel):
     message: str
     user_id: str
+
+
+class PasswordResetResponse(BaseModel):
+    email: str
+    message: str
+
+
+class PasswordResetRequest(BaseModel):
+    email: str
+
+
+class AuthTokens(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class UserCredentialsChangeResponse(BaseModel):
+    message: str
+    new_email: Optional[EmailStr]
+    new_password: Optional[str]
+
+
+class UserCredentialsChangeRequest(BaseModel):
+    new_email: Optional[EmailStr]
+    new_password: Optional[str]
