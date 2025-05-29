ff1ed8817fde24c4a9f10b69219bfd34865c2143
bani2016up <bani2016up@gmail.com>
2025-05-30 00:14:37 +0300

    maybe works

--- backend/src/app/api/services/validation.py
+++ backend/src/app/api/services/validation.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 from fastapi import HTTPException, status
 
 from app.core.dao import ValidationDAO, ErrorDAO, DocumentDAO, Validation, Document
@@ -9,13 +9,23 @@ from app.api.routes.v1.schemas.response.validation import (
 )
 from app.core.types import AsyncSession, idType
 from app.core.utils.validation import get_validation_schema
-
+from app.core.utils.ai import setup_fact_checker_model
+from AI_services.ai_services.models.fact_checker import FactCheckerPipeline
+from AI_services.ai_services.response import SuggestionResponse
 
 async def start_validation(
     document_pk: idType, sess: AsyncSession
-) -> DocumentValidationResponse:
-    #....
-    return None
+) -> List[SuggestionResponse]:
+    document: Document | None = await DocumentDAO.get(document_pk, sess)
+    if not document:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Document not found"
+        )
+    model: FactCheckerPipeline = setup_fact_checker_model()
+    predictions: List[SuggestionResponse] = model.evaluate_text(
+            document.content.lower().strip().replace("\n", " "),
+        )
+    return predictions
 
 
 
